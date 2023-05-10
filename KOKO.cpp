43cf925daeb660946c1c40bc724db265aa6e4f58
milists <128396205+milists@users.noreply.github.com>
2023-05-10 18:55:08 +0300

    Add files via upload

---
+++ KOKO.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream><time.h>
+#include "checkpole.h"
+using namespace std;
+class ko {
+public:
+	void win() {}
+	void lose() {}
+	void draw() {}
+	void inputinthefield() {}
+};
+int main() {
+	ko;
+}
---
+++ draw.h
@@ -0,0 +1,7 @@
+#pragma once
+class draw {
+public:
+	void draw() {
+		//Если void win не сработал , а на поле не осталось место то команда выводит: Ничья
+	}
+};
