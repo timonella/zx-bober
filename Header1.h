f5184cd3b38b11df2b21a4caff0c1095734e5528
forttort <128396656+forttort@users.noreply.github.com>
2023-05-10 18:54:10 +0300

    Add files via upload

---
+++ Header1.h
@@ -0,0 +1,8 @@
+#pragma once
+class IVisual {
+
+
+public:
+	virtual void Printboard() {};
+	virtual void Menu() {};
+};
---
+++ Header2.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Header1.h"
+class Visual:public IVisual {
+public:
+
+	void Printboard();
+	void Menu();
+
+};
+
